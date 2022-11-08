ad5b49a646e17c90830b21a460f874e5267d9838
Basti <ernst.sebastian1991@gmail.com>
2022-11-08 23:08:50 +0100

    Git Init

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "./markup/common-header.php";
+require_once "./markup/main-menu.php";
+require_once "./markup/header.php";
+require_once "./markup/band.php";
+require_once "./markup/merchandise.php";
+require_once "./markup/contact.php";
+require_once "./markup/common-footer.php";
---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+class MainApplication {
+    constructor() {
+
+    }
+}
+
+new MainApplication();
+
